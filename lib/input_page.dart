12fdbb5613832e613e37c12493b4b4483cef6456
ritchied <david.ritchie@argyll-bute.gov.uk>
2019-07-24 17:02:58 +0100

    about 5:50 into the video

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,8 +13,15 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        height: 200.0,
+        width: 170.0,
+        //Paused at 5:50 in How to refactor flutter widgets
       ),
     );
   }
