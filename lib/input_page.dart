436b41e742cfd8ed12cbe8c0afac335471b17de8
Dave Ritchie <ritchiedw@hotmail.com>
2019-07-24 19:14:57 +0100

    this is a very repetitive version of the format

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -11,18 +11,143 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10.0),
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(child: Text('BMI CALCULATOR')),
         ),
-        height: 200.0,
-        width: 170.0,
-        //Paused at 5:50 in How to refactor flutter widgets
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    //Paused at 5:50 in How to refactor flutter widgets
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    //Paused at 5:50 in How to refactor flutter widgets
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              //Paused at 5:50 in How to refactor flutter widgets
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    //Paused at 5:50 in How to refactor flutter widgets
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    //Paused at 5:50 in How to refactor flutter widgets
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//children: <Widget> [
+//Row(
+//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//mainAxisSize: MainAxisSize.max,
+//crossAxisAlignment: CrossAxisAlignment.center,
+//children: <Widget>[
+//Expanded(
+//child: Container(
+//child: Text("Box 1"),
+//margin: EdgeInsets.all(15.0),
+//decoration: BoxDecoration(
+//color: Color(0xFF1D1E33),
+//borderRadius: BorderRadius.circular(10.0),
+//),
+//),
+//),
+//Expanded(
+//child: Container(
+//child: Text("Box 2"),
+//margin: EdgeInsets.all(15.0),
+//decoration: BoxDecoration(
+//color: Color(0xFF1D1E33),
+//borderRadius: BorderRadius.circular(10.0),
+//),
+//),
+//),
+//],
+//),
+//Container(
+//child: Text("Box A"),
+//margin: EdgeInsets.all(15.0),
+//decoration: BoxDecoration(
+//color: Color(0xFF1D1E33),
+//borderRadius: BorderRadius.circular(10.0),
+//),
+//),
+//Row(
+//children: <Widget>[
+//Expanded(
+//child: Container(
+//child: Text("Box 1"),
+//margin: EdgeInsets.all(15.0),
+//decoration: BoxDecoration(
+//color: Color(0xFF1D1E33),
+//borderRadius: BorderRadius.circular(10.0),
+//),
+//),
+//),
+//Expanded(
+//child: Container(
+//child: Text("Box 2"),
+//margin: EdgeInsets.all(15.0),
+//decoration: BoxDecoration(
+//color: Color(0xFF1D1E33),
+//borderRadius: BorderRadius.circular(10.0),
+//),
+//),
+//),
+//],
+//),
+//],
